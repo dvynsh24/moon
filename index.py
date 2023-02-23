a52c0e7886658713a579267baf0ced41346257ad
dvynsh24 <dvynsh24@gmail.com>
2023-02-23 17:23:39 +0530

    added gender assignment

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 class human:
-    def __init__(self, name, age, height, color):
-        self.name, self.age, self.height, self.color = name, age, height, color
+    def __init__(self, name, age, height, gender, color):
+        self.name, self.age, self.height, self.gender, self.color = name, age, height, gender, color
         
     def speak(self, sentence1):
         print(f"{self.name} is going to talk about {sentence1}")
@@ -9,7 +9,8 @@ class human:
     #     print(f"{name}, {age}, {height}, {color}")
     
 
-human1=human("raj", 24, 1.8, "brown")
-human2=human("meena", 17, 1.4, "lightish brown")
+human1=human("raj", 24, 1.8, "M", "brown")
+human2=human("meena", 17, 1.4, "F", "lightish brown")
 
 print(human1.speak("this is a good life"))
+print(human2.gender)
