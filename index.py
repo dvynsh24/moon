812485ca143fbe18428aa27cded72ee030895a5f
dvynsh24 <dvynsh24@gmail.com>
2023-02-23 17:18:15 +0530

    added func checkadult

--- index.py
+++ index.py
@@ -8,8 +8,17 @@ class human:
     # def __str__(self, name, age, height, color) -> str:
     #     print(f"{name}, {age}, {height}, {color}")
     
+    def checkadult(self, age):
+        if age<18:
+            print(f"grow up and come back later, {self.name}")
+        else:
+            print(f"nice, you are perfectly ripe, {self.name}")
+    
 
 human1=human("raj", 24, 1.8, "brown")
 human2=human("meena", 17, 1.4, "lightish brown")
 
+# printing the sentence
 print(human1.speak("this is a good life"))
+print(human1.checkadult(human1.age))
+
